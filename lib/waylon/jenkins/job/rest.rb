uchuugaka/waylon

commit 9badacc0fa8afa7d3564fae1223b299f53cdca79

linting fixes

lib/waylon/jenkins/job/rest.rb
@@ -31,7 +31,7 @@ class Waylon
 
         def progress_pct
           # Note that 'timestamp' available the Jenkins API is returned in ms
-          start_time = client.api_get_request("/job/#{@name}/lastBuild", nil, '/api/json?depth=1&tree=timestamp')['timestamp'] / 1000.0
+          start_time   = client.api_get_request("/job/#{@name}/lastBuild", nil, '/api/json?depth = 1&tree = timestamp')['timestamp'] / 1000.0
           progress_pct = ((Time.now.to_i - start_time) / est_duration) * 100
 
           # The above math isn't perfect, and Jenkins is probably a bit janky.
@@ -56,7 +56,7 @@ class Waylon
           # after it has completed. Using estimatedDuration and the
           # executor progress (in percentage), we can calculate the ETA.
           if progress_pct != -1 then
-            t = (est_duration - (est_duration * (progress_pct / 100.0)))
+            t      = (est_duration - (est_duration * (progress_pct / 100.0)))
             mm, ss = t.divmod(60)
             return "#{mm}m #{ss.floor}s"
           else

lib/jenkins_api_client/core_ext.rb
@@ -1,5 +1,5 @@
 # Extend the Jenkins API client for nexted views.
-# @see https://wiki.jenkins-ci.org/display/JENKINS/Nested+View+Plugin
+# See https://wiki.jenkins-ci.org/display/JENKINS/Nested+View+Plugin
 
 require_relative 'nested_view'
 require_relative 'client'

lib/waylon/config/root_config.rb
@@ -9,7 +9,7 @@ class Waylon
 
       def self.from_hash(hash)
         new.tap do |x|
-          x.views  = hash['views'].map { |kvp| Waylon::Config::View.from_hash(*kvp) }
+          x.views      = hash['views'].map { |kvp| Waylon::Config::View.from_hash(*kvp) }
           x.app_config = Waylon::Config::AppConfig.from_hash(hash['config'] || {})
         end
       end

lib/waylon/config/server.rb
@@ -34,13 +34,13 @@ class Waylon
       end
 
       def initialize(name, url, username, password)
-        @name = name
-        @url = url
+        @name     = name
+        @url      = url
         @username = username
         @password = password
 
-        @job_names = []
-        @view_names = []
+        @job_names         = []
+        @view_names        = []
         @nested_view_names = {}
       end
 

lib/waylon/config/view.rb
@@ -19,7 +19,7 @@ class Waylon
       end
 
       def initialize(name)
-        @name = name
+        @name    = name
         @servers = []
       end
     end

lib/waylon/jenkins/job.rb
@@ -8,7 +8,7 @@ class Waylon
       attr_reader :server
 
       def initialize(name, server)
-        @name = name
+        @name   = name
         @server = server
       end
     end

lib/waylon/jenkins/view.rb
@@ -3,7 +3,7 @@ class Waylon
     class View
       attr_accessor :servers
       def initialize(config)
-        @config = config
+        @config  = config
         @servers = []
       end
 
@@ -27,7 +27,7 @@ class Waylon
 
       def to_config
         {
-          'name' => name,
+          'name'    => name,
           'servers' => Hash[(@servers.map { |s| [s.name, s.url] })]
         }
       end

commit 50932c67082f331be11dcd6b9704d75c008eb5aa

(fixup) fix eta query broken by 9badacc
In commit 9badacc, we applied a number of linting fixes to the codebase.
However, in rest.rb we inadvertently introduced spaces into the query
URI, breaking ETAs across the board. This commit introduces a fixup to
the query URI so that ETAs can be calculated again.

lib/waylon/jenkins/job/rest.rb
@@ -31,7 +31,7 @@ class Waylon
 
         def progress_pct
           # Note that 'timestamp' available the Jenkins API is returned in ms
-          start_time   = client.api_get_request("/job/#{@name}/lastBuild", nil, '/api/json?depth = 1&tree = timestamp')['timestamp'] / 1000.0
+          start_time   = client.api_get_request("/job/#{@name}/lastBuild", nil, '/api/json?depth=1&tree=timestamp')['timestamp'] / 1000.0
           progress_pct = ((Time.now.to_i - start_time) / est_duration) * 100
 
           # The above math isn't perfect, and Jenkins is probably a bit janky.